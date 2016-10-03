92bb1949180dc2a34b765a869a09eabd4bc350b2
blitztech4@yahoo.com <blitztech4@yahoo.com>
2016-10-04 05:30:26 +0800

    fix sql/updates/world/6.x/9999_99_99_10_world.sql

--- sql/updates/world/6.x/9999_99_99_10_world.sql
+++ sql/updates/world/6.x/9999_99_99_10_world.sql
@@ -1,4 +1,3 @@
---Core/Spells: Fix up Rogues Stealth--
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (1784,115191) AND `spell_effect` IN (158185,158188);
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 (1784,158185,2,'Stealth mod'),
