be515ba630880ee89566ae02fd4ae292365c2ccd
Ex pert <blitztech4@yahoo.com>
2016-10-22 21:17:55 +0800

    Draenor / Pandaria Script

---
+++ src/server/scripts/Draenor/draenor_script_loader.cpp
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/* raid Highmaul */
+void AddSC_boss_Brackenspore();
+void AddSC_boss_KargathBladefist();
+void AddSC_boss_Koragh();
+void AddSC_boss_Margok();
+void AddSC_boss_Tectus();
+void AddSC_boss_TheButcher();
+void AddSC_boss_TwinOgron();
+void AddSC_Highmaul_doorlevers();
+void AddSC_instance_Highmaul();
+
+/* raid BlackrockFoundry */
+void AddSC_boss_Blackhand();
+void AddSC_boss_Darmac();
+void AddSC_boss_Gruul();
+void AddSC_boss_HansgarAndFranzok();
+void AddSC_boss_Kagraz();
+void AddSC_boss_Kromog();
+void AddSC_boss_Oregorger();
+void AddSC_boss_TheBlastFurnace();
+void AddSC_boss_TheIronMaidens();
+void AddSC_boss_Thogar();
+void AddSC_instance_BlackrockFoundry();
+
+void AddSC_SpiresofArak_doorlevers();
+
+void AddSC_Boss_SlaveWatcherCrushto();
+void AddSC_Boss_Roltall();
+void AddSC_Boss_Gugrokk();
+void AddSC_Boss_Magmolatus();
+void AddSC_instance_BloodmaulSlagMines();
+
+void AddSC_boss_fleshrender_nokgar();
+void AddSC_boss_ahriok_dugru();
+void AddSC_boss_oshir();
+void AddSC_boss_skulloc();
+void AddSC_boss_GrimrailEnforcers();
+
+/* Auchindoun */
+void AddSC_instance_Auchindoun();
+void AddSC_boss_vigilant_kaathar();
+void AddSC_boss_soulbinder_nyami();
+void AddSC_boss_azzakel();
+void AddSC_boss_terongor();
+
+void AddSC_boss_ranjit();
+void AddSC_boss_araknath();
+void AddSC_boss_high_sage_viryx();
+void AddSC_boss_rukhran();
+void AddSC_instance_Skyreach();
+
+void AddSC_boss_bonemaw();
+void AddSC_boss_nerzhul();
+void AddSC_boss_nhallish();
+void AddSC_boss_sadana_bloodfury();
+void AddSC_instance_ShadowmoonBurialGrounds();
+
+void AddSC_boss_nitrogg_thundertower();
+void AddSC_boss_railmaster_rocketspark();
+void AddSC_boss_skylord_tovra();
+void AddSC_instance_GrimrailDepot();
+void AddSC_boss_archmage_sol();
+void AddSC_boss_dulhu();
+void AddSC_boss_witherbark();
+void AddSC_boss_xeritac();
+void AddSC_boss_yalnu();
+void AddSC_instance_TheEverbloom();
+
+void AddSC_boss_commander_tharbek();
+void AddSC_boss_kyrak();
+void AddSC_boss_orebender_gorashan();
+void AddSC_boss_ragewing_the_untamed();
+void AddSC_boss_warlord_zaela();
+void AddSC_instance_UpperBlackrockSpire();
+
+void AddSC_Bonemaw_doorlevers();
+void AddSC_worldboss_Drov();
+void AddSC_worldboss_Rukhmar();
+void AddSC_worldboss_Tarlna();
+void AddSC_Garrison_Frostwall();
+
+void AddDraenorScripts()
+{
+	/* Bloodmaul Slag Mines */
+	AddSC_Boss_SlaveWatcherCrushto();
+	AddSC_Boss_Roltall();
+	AddSC_Boss_Gugrokk();
+	AddSC_Boss_Magmolatus();
+	AddSC_instance_BloodmaulSlagMines();
+
+	/* Iron Docks */
+	AddSC_boss_fleshrender_nokgar();
+	AddSC_boss_ahriok_dugru();
+	AddSC_boss_oshir();
+	AddSC_boss_skulloc();
+	AddSC_boss_GrimrailEnforcers();
+
+	/* Auchindoun */
+	AddSC_instance_Auchindoun();
+	AddSC_boss_vigilant_kaathar();
+	AddSC_boss_soulbinder_nyami();
+	AddSC_boss_azzakel();
+	AddSC_boss_terongor();
+
+	/* Sky Reach */
+	AddSC_boss_ranjit();
+	AddSC_boss_araknath();
+	AddSC_boss_high_sage_viryx();
+	AddSC_boss_rukhran();
+	AddSC_instance_Skyreach();
+
+	/* Shadowmoon Burial Grounds */
+	AddSC_boss_bonemaw();
+	AddSC_boss_nerzhul();
+	AddSC_boss_nhallish();
+	AddSC_boss_sadana_bloodfury();
+	AddSC_instance_ShadowmoonBurialGrounds();
+
+	/* Grimrail Depot */
+	AddSC_boss_nitrogg_thundertower();
+	AddSC_boss_railmaster_rocketspark();
+	AddSC_boss_skylord_tovra();
+	AddSC_instance_GrimrailDepot();
+
+	/* The Everbloom */
+	AddSC_boss_archmage_sol();
+	AddSC_boss_dulhu();
+	AddSC_boss_witherbark();
+	AddSC_boss_xeritac();
+	AddSC_boss_yalnu();
+	AddSC_instance_TheEverbloom();
+
+	/* Upper Blackrock Spire */
+	AddSC_boss_commander_tharbek();
+	AddSC_boss_kyrak();
+	AddSC_boss_orebender_gorashan();
+	AddSC_boss_ragewing_the_untamed();
+	AddSC_boss_warlord_zaela();
+	AddSC_instance_UpperBlackrockSpire();
+	
+	AddSC_Bonemaw_doorlevers();
+	AddSC_worldboss_Drov();
+	AddSC_worldboss_Drov();
+    AddSC_worldboss_Rukhmar();
+    AddSC_worldboss_Tarlna();
+	AddSC_Garrison_Frostwall();
+	
+	/* raid BlackrockFoundry*/
+	AddSC_boss_Blackhand();
+	AddSC_boss_Darmac();
+	AddSC_boss_Gruul();
+	AddSC_boss_HansgarAndFranzok();
+	AddSC_boss_Kagraz();
+	AddSC_boss_Kromog();
+	AddSC_boss_Oregorger();
+	AddSC_boss_TheBlastFurnace();
+	AddSC_boss_TheIronMaidens();
+	AddSC_boss_Thogar();
+	AddSC_instance_BlackrockFoundry();
+	
+	/* raid Highmaul */
+	AddSC_boss_Brackenspore();
+	AddSC_boss_KargathBladefist();
+	AddSC_boss_Koragh();
+	AddSC_boss_Margok();
+	AddSC_boss_Tectus();
+	AddSC_boss_TheButcher();
+	AddSC_boss_TwinOgron();
+	AddSC_Highmaul_doorlevers();
+	AddSC_instance_Highmaul();
+	
+	AddSC_SpiresofArak_doorlevers();
+}
