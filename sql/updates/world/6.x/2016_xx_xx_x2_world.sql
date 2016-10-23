d4fb27e147620bcd135f2e0f92451776e85c6ee3
blitztech4@yahoo.com <blitztech4@yahoo.com>
2016-10-23 16:27:29 +0800

    Revert "Scripts/Spells: Fix Shaman Talent Ancestral Guidance"
    This reverts commit 6d86459ad3b29d66d3ff7fd161cc9f1e67c6129f.

--- sql/updates/world/6.x/2016_xx_xx_x2_world.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` IN (108281, 114911);
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) values
-(108281,'spell_sha_ancestral_guidance'),
-(114911,'spell_sha_ancestral_guidance_heal');
